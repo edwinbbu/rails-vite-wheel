d7d4632cc34a12383cf8cec0aab1c8d008a2f865
edwinbbu <edwinbbu@gmail.com>
2022-11-08 21:51:31 +0530

    React controller connected

---
+++ app/javascript/controllers/react_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="react"
+export default class extends Controller {
+  connect() {
+    console.log('React controller connected');
+  }
+}
