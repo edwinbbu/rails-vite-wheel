ec8c6533be16cb93efbd5938db66369b5b3c4a6b
edwinbbu <edwinbbu@gmail.com>
2022-11-11 10:51:46 +0530

    Fixed tailwind setup

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const App = () => {
   return (
-    <div className='text-5xl'>React with esbuild, hotwire and stimulus</div>
+    <div className='text-5xl text-blue-600'>React with esbuild, hotwire and tailwindcss</div>
   )
 }
