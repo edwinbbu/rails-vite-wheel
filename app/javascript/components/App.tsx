1b169505e168fc41c2c603ccee94de4f94429592
edwinbbu <edwinbbu@gmail.com>
2023-06-08 11:26:08 +0530

    ts changes

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const App = () => {
   return (
     <div className="text-5xl text-blue-600">
-      React with Vite and tailwindcss
+      React with ts, vite and tailwindcss.
     </div>
   );
 };
