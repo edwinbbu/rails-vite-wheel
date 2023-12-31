92dc31ae0ac1d0809b534645f8a276d8d4c55717
edwinbbu <edwinbbu@gmail.com>
2023-12-31 12:46:46 +0530

    Resolved conflict

--- app/javascript/entrypoints/index.tsx
+++ app/javascript/entrypoints/index.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "../components/App";
-<<<<<<< HEAD
-=======
 import "./styles.scss";
->>>>>>> 7011ce6 (Tailwind style fix)
 
 ReactDOM.render(
   <React.StrictMode>
