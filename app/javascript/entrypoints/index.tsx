a9c3c46232263f0fd2dda52da8c295c464a34806
edwinbbu <edwinbbu@gmail.com>
2023-12-31 12:42:17 +0530

    Tailwind style fix

--- app/javascript/entrypoints/index.tsx
+++ app/javascript/entrypoints/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "../components/App";
+<<<<<<< HEAD
+=======
+import "./styles.scss";
+>>>>>>> 7011ce6 (Tailwind style fix)
 
 ReactDOM.render(
   <React.StrictMode>
