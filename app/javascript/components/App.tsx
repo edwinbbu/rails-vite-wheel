8da422a127f91a6607ca84146a7752b0ba2f9ee0
edwinbbu <edwinbbu@gmail.com>
2023-12-31 12:47:09 +0530

    Resolved conflict

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -2,13 +2,8 @@ import React from "react";
 
 export const App = () => {
   return (
-<<<<<<< HEAD
-    <div className="text-5xl text-blue-600">
-      React with ts, vite and tailwindcss.
-=======
     <div className="p-6 container mx-auto">
       <div className="text-5xl text-indigo-700 pb-4">Rails vite wheel</div>
->>>>>>> 7011ce6 (Tailwind style fix)
     </div>
   );
 };
